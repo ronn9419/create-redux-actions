91f49fa787b785a5e1dc26a0f68a34cb828a34a1
Ronald Gipa <ronald@invento.io>
2019-01-25 02:55:49 -0800

    Build v0.1.2

--- es/handleActions.js
+++ es/handleActions.js
@@ -42,8 +42,7 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var handlersKeyMap = (_handlersKeyMap = {}, _defineProperty(_handlersKeyMap, _handleNestedActions.key, _handleNestedActions.nestedHandler), _defineProperty(_handlersKeyMap, _handleCombineActions.key, _handleCombineActions.combineActionsHandler), _defineProperty(_handlersKeyMap, _handleDynamicState.key, _handleDynamicState.pathHandler), _defineProperty(_handlersKeyMap, _handleApiRequest.key, _handleApiRequest.apiRequestHandler), _handlersKeyMap); // Create-redux-actions module name
-
+var handlersKeyMap = (_handlersKeyMap = {}, _defineProperty(_handlersKeyMap, _handleNestedActions.key, _handleNestedActions.nestedHandler), _defineProperty(_handlersKeyMap, _handleCombineActions.key, _handleCombineActions.combineActionsHandler), _defineProperty(_handlersKeyMap, _handleDynamicState.key, _handleDynamicState.pathHandler), _defineProperty(_handlersKeyMap, _handleApiRequest.key, _handleApiRequest.apiRequestHandler), _handlersKeyMap);
 /**
  *
  * @param {*} handlers
@@ -113,137 +112,6 @@ var _default = function _default(handlers) {
   };
 
   return handlerFn;
-}; // // use case 1
-// // DEPRECATED: stick to original flow. just use mergeDeep to merge recursively
-// const reducer = handleActions({
-//     [creator]: (state, action) => {
-//         // return only what was changed
-//         return {
-//             pending: false
-//         }
-//     }
-// }, initialState)
-// // use case 2
-// // DONE
-// // supports nested actions
-// const reducer = handleActions({
-//     [nested(creator)]: {
-//          // function only
-//         do: (state, action) => ({
-//             done: false
-//         }),
-//          // function and nested
-//         undo: [
-//             (state, action) => ({
-//                 done: false
-//             }),
-//             {
-//                 latest: (state, action) => ({
-//                     latest: true
-//                 }),
-//             }
-//         ],
-//          // nested only
-//         redo: {
-//             first: () => ({
-//             })
-//         }
-//     }
-// }, initialState)
-// use case 3
-// handles action.request, success, abort, error, etc
-// defaultInitialState =
-// {
-//     error: false,
-//     data: [],
-//     pending: false
-// }
-// optional, will be merged to defaultInitialState
-// const overrideInitialState = {
-//     error: null,
-//     data: null,
-//     pending: true,
-//     // ...other
-// }
-// const reducer = handleWebApiActions(fetchProfile, {
-//     // override default on api request handler, optional
-//     [fetchProfile]: {
-//         // nextState will only be available for api request actions
-//         request: (state, action, nextState) => ({
-//             ...nextState,
-//         }),
-//         success: (state, action, nextState) => ({
-//             ...nextState,
-//         }),
-//         error: (state, action, nextState) => ({
-//             ...nextState,
-//         }),
-//     },
-//     [otherAction]: (state, action) => ({
-//         ...state
-//     })
-// }, overrideInitialState)
-// // use case 4
-// // DONE
-// // multiple action
-// const reducer = handleActions({
-//     // will be called if either one of them was dispatched
-//     [combineActions'action1', 'action2')]: (state, action) => ({
-//         isOpen: false
-//     })
-// }, initialState)
-// // use case 5
-// // dynamic state
-// const reducer = handleActions({
-//     [path(['sessions', 'selected'])]: {
-//         // state is whatever on sessions.selected
-//         [action1]: (state, action) => ({
-//             selected: null
-//         })
-//     },
-//     [path(['sessions', action => action.meta.id])]: {
-//         // state is whatever on sessions[action.meta.id]
-//         // return of undefined must delete the last path from state
-//         [action2]: (state, action) => undefined
-//     }
-// }, initialState)
-// // use case 6
-// // apiRequest actions
-// const reducer = handleActions(
-//     {
-//         // 'key', actionCreator, overrideApiInitialState = { error: false, pending: false, data: []}
-//         [apiRequest('profile', fetchProfile, overrideApiInitialState)]: {
-//             // overrides default success handler, optional
-//             // same params to handleWebApiActions
-//             success: (state, action, nextState) => ({
-//                 ...nextState,
-//                 data: action.payload.data
-//             })
-//         },
-//         // empty object. default, error, pending, success handler
-//         [apiRequest('images', fetchImages, overrideApiInitialState)]: {},
-//         // optional, can edit root reducer state
-//         [fetchImages.request]: (state, action) => ({
-//             ...nextState
-//         })
-//     },
-//     initialState
-// )
-// Must support nested custom handlers
-// ex.
-// {
-//      [path('session', 'selected')]: {
-//          combineActions'')
-//      }
-//
-//      [nested(action)]: {
-//          [path('hey', 'boy')]: {
-//                {
-//
-//                }
-//          }
-//      }
-// }
-
+};
 
 exports.default = _default;
